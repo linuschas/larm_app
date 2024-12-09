ddc4bb6a54e84eb1721e55bf3545770e53b34187
LindeborgLove <lindeborglove@gmail.com>
2024-12-09 12:03:16 +0100

    Created implementations of sql insert methods

--- src/SQLDatabase/SQLDatabase.cpp
+++ src/SQLDatabase/SQLDatabase.cpp
@@ -14,38 +14,72 @@ SQLDatabase::SQLDatabase(std::string path)
     }
 
     sqlite3_exec(db, 
-        "CREATE TABLE IF NOT EXISTS alarm (address TEXT, customerName TEXT, PRIMARY KEY (address, customerName));",
+        "CREATE TABLE IF NOT EXISTS alarm (address TEXT NOT NULL, customerName TEXT NOT NULL, PRIMARY KEY (address, customerName));",
         nullptr, nullptr, nullptr
     );
 
     sqlite3_exec(db, 
-        "CREATE TABLE IF NOT EXISTS alarmComponent (id INTEGER PRIMARY KEY AUTOINCREMENT, \
-            componentType TEXT, name TEXT, isActive BOOLEAN, address TEXT, \
-            FOREIGN KEY (address) REFERENCES alarm (address) ON DELETE CASCADE));",
+        "CREATE TABLE IF NOT EXISTS customer (id INTEGER PRIMARY KEY AUTOINCREMENT, \
+            name TEXT NOT NULL, pinCode TEXT(4) NOT NULL, tagId TEXT UNIQUE NOT NULL, verificationPhrase TEXT UNIQUE NOT NULL, `address` TEXT NOT NULL, \
+            FOREIGN KEY (`address`) REFERENCES alarm(`address`) ON DELETE CASCADE);",
         nullptr, nullptr, nullptr
     );
 
     sqlite3_exec(db, 
-        "CREATE TABLE IF NOT EXISTS customers (id INTEGER PRIMARY_KEY AUTOINCREMENT, \
-            name TEXT, pinCode TEXT(4), tagId TEXT UNIQUE KEY, verificationPhrase TEXT UNIQUE KEY, address TEXT, \
-            FOREIGN KEY (address) REFERENCES alarm(address) ON DELETE CASCADE);",
+        "CREATE TABLE IF NOT EXISTS alarmComponent (                                    \
+            id INTEGER PRIMARY KEY AUTOINCREMENT,                                       \
+            componentType TEXT NOT NULL,                                                \
+            owner TEXT NOT NULL,                                                         \
+            isActive BOOLEAN DEFAULT FALSE NOT NULL,                                    \
+            address TEXT NOT NULL,                                                      \
+        FOREIGN KEY (address) REFERENCES alarm (address) 	ON DELETE CASCADE,          \
+        FOREIGN KEY (owner) REFERENCES customer (name) 	    ON DELETE CASCADE);",
         nullptr, nullptr, nullptr
     );  
 };
 
-inline int SQLDatabase::insertAlarmSystem(AlarmSystem alarm)
+inline int SQLDatabase::insertAlarmSystem(AlarmSystem &alarm)
 {
-    return 0;
+    sqlite3_stmt *stmt;
+    std::string sql = "INSERT INTO alarm (address, customerName) VALUES (?, ?)";
+    sqlite3_prepare(db, sql.c_str(), sql.size(), &stmt, nullptr);
+
+    sqlite3_bind_text(stmt, 1, alarm.address.c_str(), alarm.address.size(), nullptr);
+    sqlite3_bind_text(stmt, 2, alarm.customerName.c_str(), alarm.customerName.size(), nullptr);
+
+    sqlite3_step(stmt);
+    return sqlite3_finalize(stmt);
 }
 
-inline int SQLDatabase::insertAlarmComponent(AlarmComponent alarm)
+inline int SQLDatabase::insertAlarmComponent(AlarmComponent &alarm)
 {
-    return 0;
+    sqlite3_stmt *stmt;
+    std::string sql = "INSERT INTO alarmComponent (address, componentType, owner) VALUES (?, ?, ?)";
+    const char* errorMsg;
+    sqlite3_prepare(db, sql.c_str(), sql.size(), &stmt, &errorMsg);
+
+    sqlite3_bind_text(stmt, 1, alarm.address.c_str(), alarm.address.size(), nullptr);
+    sqlite3_bind_text(stmt, 2, alarm.componentType.c_str(), alarm.componentType.size(), nullptr);
+    sqlite3_bind_text(stmt, 3, alarm.owner.c_str(), alarm.owner.size(), nullptr);
+
+    sqlite3_step(stmt);
+    return sqlite3_finalize(stmt);
 }
 
-inline int SQLDatabase::insertCustomer(Customer alarm)
+inline int SQLDatabase::insertCustomer(Customer &alarm)
 {
-    return 0;
+    sqlite3_stmt *stmt;
+    std::string sql = "INSERT INTO customer (address, pinCode, tagId, verificationPhrase, name) VALUES (?, ?, ?, ?, ?)";
+    sqlite3_prepare(db, sql.c_str(), sql.size(), &stmt, nullptr);
+
+    sqlite3_bind_text(stmt, 1, alarm.address.c_str(), alarm.address.size(), nullptr);
+    sqlite3_bind_text(stmt, 2, alarm.pinCode.c_str(), alarm.pinCode.size(), nullptr);
+    sqlite3_bind_text(stmt, 3, alarm.tagId.c_str(), alarm.tagId.size(), nullptr);
+    sqlite3_bind_text(stmt, 4, alarm.verificationPhrase.c_str(), alarm.verificationPhrase.size(), nullptr);
+    sqlite3_bind_text(stmt, 5, alarm.name.c_str(), alarm.name.size(), nullptr);
+
+    sqlite3_step(stmt);
+    return sqlite3_finalize(stmt);
 }
 
 inline int SQLDatabase::getAlarmSystem(AlarmSystem alarm)
