1d673d6563482b25e6f44c7669cd65922c15954b
islandskan <sigridur.eggertsdottir@chasacademy.se>
2024-12-09 10:22:38 +0100

    add simple test

--- src/LarmComponent/test.cpp
+++ src/LarmComponent/test.cpp
@@ -2,7 +2,7 @@
 #include "catch.hpp"
 #include "LarmComponent.h"
 
-    LarmComponent larm_component(21321, "sensor","0732.1", false, "324 556");
+    LarmComponent larm_component{21321, "sensor","0732.1", false, "324 556"};
     std::string address = larm_component.get_address();
     std::string name = larm_component.get_component_name();
     std::string type = larm_component.get_component_type();
