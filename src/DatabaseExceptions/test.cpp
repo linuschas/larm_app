c72a377e14ef108cbc1be23db772341e2e3238e2
LindeborgLove <lindeborglove@gmail.com>
2024-12-09 16:28:15 +0100

    Made tests

---
+++ src/DatabaseExceptions/test.cpp
@@ -0,0 +1,37 @@
+#include <catch2/catch_test_macros.hpp>
+
+#define DEBUG
+
+#include "CouldNotCreateDatabase.h"
+#include "CouldNotInsert.h"
+#include "SQLDatabase/SQLDatabase.h"
+
+TEST_CASE("Testing Database exceptions", "[Exceptions],[Database]") 
+{
+    SQLDatabase database{"test_database.sql"};
+    database.clearTables();
+
+    AlarmSystem system; 
+    system.customerName = "Boys";
+    system.address = "Backstreet";
+    
+    LarmComponent component;
+    component.address = "Backstreet";
+    component.name = "Boys";
+    component.componentType = "Person";
+
+    Customer customer;
+    customer.address = "Backstreet";
+    customer.name = "Boys";
+    customer.pinCode = "1234";
+    customer.tagId = "dhy562187213afYgajHipKjhh1";
+    customer.verificationPhrase = "Backstreets back";
+
+    REQUIRE(database.insertAlarmSystem(system) == SQLITE_OK);
+    REQUIRE(database.insertAlarmComponent(component) == SQLITE_OK);
+    REQUIRE(database.insertCustomer(customer) == SQLITE_OK);
+    std::cout << "Fuck" << std::endl;
+
+    REQUIRE_THROWS_AS(database.insertAlarmSystem(system), CouldNotInsert);
+    REQUIRE_THROWS_AS(database.insertCustomer(customer), CouldNotInsert);
+}
