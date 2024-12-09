b6b4dcf464c43295a4dc78bc312aa0d5aa8c6b90
LindeborgLove <lindeborglove@gmail.com>
2024-12-09 09:20:04 +0100

    Added SQLite sub-class

---
+++ src/SQLDatabase/SQLDatabase.h
@@ -0,0 +1,22 @@
+#ifndef __SQL_DATABASE_H__
+#define __SQL_DATABASE_H__
+
+#include <sqlite3.h>
+#include "Database.h"
+
+class SQLDatabase : Database
+{
+public:
+    virtual int insertAlarmSystem(AlarmSystem alarm) override;
+    virtual int insertAlarmComponent(AlarmComponent alarm) override;
+    virtual int insertCustomer(Customer alarm) = 0;
+
+    virtual int getAlarmSystem(AlarmSystem alarm) override;
+    virtual int getAlarmComponent(AlarmComponent alarm) override;
+    virtual int getCustomer(Customer alarm) override;
+
+private:
+    sqlite3 *db;
+};
+
+#endif
