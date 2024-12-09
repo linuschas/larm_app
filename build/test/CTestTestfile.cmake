02987526a9c5be9b952597eb57f7d6c83565c41a
LindeborgLove <lindeborglove@gmail.com>
2024-12-09 10:45:14 +0100

    Removed build files

--- build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lovel/Repositories/larm_app/test
-# Build directory: /home/lovel/Repositories/larm_app/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/lovel/Repositories/larm_app/build/test/test-b12d07c_include.cmake")
