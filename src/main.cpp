5e853a67679a1ef366b88e91482f7f9b9853dcdd
LindeborgLove <lindeborglove@gmail.com>
2024-12-09 12:03:26 +0100

    Added testing

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,23 @@
 
 int main() {
     SQLDatabase database{"database.sql"};
+    AlarmSystem system; 
+    system.customerName = "Boys";
+    system.address = "Backstreet";
+    database.insertAlarmSystem(system);
+    
+    AlarmComponent component;
+    component.address = "Backstreet";
+    component.owner = "Boys";
+    component.componentType = "Person";
+    database.insertAlarmComponent(component);
+
+    Customer customer;
+    customer.address = "Backstreet";
+    customer.name = "Boys";
+    customer.pinCode = "1234";
+    customer.tagId = "dhy562187213afYgajHipKjhh1";
+    customer.verificationPhrase = "Backstreets back";
+    database.insertCustomer(customer);
     return 0;
 }
